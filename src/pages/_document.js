783dcb2b5f5612e461dc03ed74bb441cddc3ff1b
MbaliPuso <mbalipuso@gmail.com>
2025-02-21 06:57:05 +0200

    update

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -12,8 +12,8 @@ class MyDocument extends Document {
 
           {/* favicon begin */}
           <link rel="apple-touch-icon" sizes="76x76" href="/favicon/apple-touch-icon.png" />
-          <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
-          <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon.png" />
           <link rel="manifest" href="/favicon/manifest.json" />
           <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#5bbad5" />
           <meta name="msapplication-TileColor" content="#da532c" />
