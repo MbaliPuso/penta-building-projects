60479827ce64500c15242e8adb5e9134cd36bf36
MbaliPuso <mbalipuso@gmail.com>
2025-02-24 16:36:03 +0200

    update

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e34fc69fcd691a84"; }
+/******/ 		__webpack_require__.h = function() { return "01030bc4bf0896dd"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
